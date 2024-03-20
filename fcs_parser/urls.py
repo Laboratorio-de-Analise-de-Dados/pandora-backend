8134aa89509b4c4b3315607ed6a61e649ce3d8d6
Paulo Moro <paulo.morolol@gmail.com>
2024-03-20 16:12:28 -0300

    refactor: list experiment, get experiment files and list file params endpoints

--- fcs_parser/urls.py
+++ fcs_parser/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ExperimentCreateView
+from .views import ExperimentCreateView, GetExperimentFiles, ListExperimentView, ListFileParams
 app_name = 'fcs_parse'
 
 urlpatterns = [
-    path('create', ExperimentCreateView.as_view()),
+    path('list/', ListExperimentView.as_view()),
+    path('create/', ExperimentCreateView.as_view()),
+    path('list/data/<str:experiment_id>/', GetExperimentFiles.as_view()),
+    path('file/<str:file_id>/list', ListFileParams.as_view())
 ]
