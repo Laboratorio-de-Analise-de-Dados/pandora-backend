9482936000ead73632e83fc7d3f834bc2ddc5886
Paulo Moro <paulo.morolol@gmail.com>
2024-03-20 12:09:52 -0300

    feat:create experiment model and filedata model on fcs_parser app

--- fcs_parser/models.py
+++ fcs_parser/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 
-from django.core.exceptions import ValidationError
-
-MB = 5
-MAX_SIZE = MB * 1024 * 1024
-
-
-def validate_file_size(file):
-  """Validation file size function, take a file as argument"""
-
-  dir(file)
-  if file.size > MAX_SIZE:
-      raise ValidationError(f"File exceed maximum size {MB}mb")
-
-class FCSFile(models.Model):
+class ExperimentModel(models.Model):
     """Model for FCS file"""
-
+    id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=50, unique=True)
-    file = models.FileField(upload_to='fcs_files/', validators=[validate_file_size])
+
+    class Meta:
+      db_table='experiment'
+
+class FileDataModel(models.Model):
+  """Model for Data on each file"""
+  id = models.BigAutoField(primary_key=True)
+  experiment = models.ForeignKey(ExperimentModel, on_delete=models.CASCADE)
+  headers = models.JSONField()
+  data_set = models.JSONField()
+  
+  class Meta:
+      db_table='file_data'
+
+  def is_valid(self, raise_exception=False):
+    headers = self.initial_data.get('headers')
+    data_set = self.initial_data.get('data_set')
+
+    if not self.validate_json_field(headers):
+        self.errors['headers'] = ['Invalid field headers.']
+    if not self.validate_json_field(data_set):
+        self.errors['data_set'] = ['Invalid field data_set.']
+
+    return super().is_valid(raise_exception)
 
 
