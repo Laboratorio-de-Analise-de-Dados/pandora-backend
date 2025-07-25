0570fd12af578d2c4c198138d2f2ffefd43e22ae
Paulo Moro <paulo.morolol@gmail.com>
2025-07-25 07:51:47 -0300

    chore: add missing libraries to requirements.txt

--- check_migration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Testa se o comando de migração ainda está rodando
-if [ "$(ps aux | grep '[p]ython manage.py migrate' | wc -l)" -eq "0" ]; then
-  exit 0 # Serviço saudável
-else
-  exit 1 # Serviço ainda está rodando
-fi
